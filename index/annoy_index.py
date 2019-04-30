df1d198b5da6df6622ca420b561575a10a59f682
Nadja <nadja.b.kurz@student.hs-rm.de>
2019-04-30 16:34:00 +0200

    Updated the Indexer to make it easier to add other embedding models than sent2vec

--- index/annoy_index.py
+++ index/annoy_index.py
@@ -1,22 +1,27 @@
 import annoy
 import lmdb
 import sent2vec
+import logging
+import numpy as np
 
 from pathlib import Path
 from typing import Set, List
+from abc import ABCMeta, abstractmethod
+
+log = logging.getLogger(__name__)
 
 
-class AnnoyIndexer:
-    def __init__(self, embedding_model_path: str, embedding_model_type: str):
+class AnnoyIndexer(metaclass=ABCMeta):
+    def __init__(self, embedding_model, embedding_vector_size):
         self._annoy_index = None
         self._annoy_mapping = None
 
-        assert embedding_model_type in ['sent2vec']
-        self._embedding_model_type = embedding_model_type
-        if self._embedding_model_type == 'sent2vec':
-            self._embedding_model = sent2vec.Sent2vecModel()
-            self._embedding_model.load_model(embedding_model_path)
-            self._embedding_vector_size = self._embedding_model.get_emb_size()
+        self._embedding_model = embedding_model
+        self._embedding_vector_size = embedding_vector_size
+
+    @abstractmethod
+    def _get_embedding(self, phrase: str) -> List[float]:
+        pass
 
     def create_entity_index(self, entities: Set[str], output_filename: str, num_trees: int=30):
         file_annoy = output_filename + ".ann"
@@ -28,22 +33,19 @@ class AnnoyIndexer:
         # Create mapping instance
         self._annoy_mapping = lmdb.open(file_lmdb, map_size=int(1e9))
 
-        # Get (sent2vec) embeddings for all entities
+        # Get embeddings for all entities
         with self._annoy_mapping.begin(write=True) as mapping:
             for entity_id, entity in enumerate(entities):
                 # Replace _ symbols with spaces
                 entity = str(entity)
                 refactored_entity = entity.replace("_", " ")
-
-                if self._embedding_model_type == 'sent2vec':
-                    emb = self._embedding_model.embed_sentence(refactored_entity)[0]
+                emb = self._get_embedding(refactored_entity)
 
                 # Add entity embedding to index
                 self._annoy_index.add_item(entity_id, emb)
 
                 # Add ID <-> word mapping
                 mapping.put(str(entity_id).encode(), entity.encode())
-                print(entity, entity_id)
 
         # Build annoy index
         self._annoy_index.build(num_trees)
@@ -68,7 +70,7 @@ class AnnoyIndexer:
 
         self._annoy_mapping = lmdb.open(file_lmdb, map_size=int(1e9))
 
-    def get_nns_by_vector(self, emb_vector: List[float], num_nn: int) -> Set[str]:
+    def get_nns_by_vector(self, emb_vector: List[float], num_nn: int) -> List[str]:
         """
         Returns a set of nearest neighbour entities for the given embedding vector.
         """
@@ -76,14 +78,14 @@ class AnnoyIndexer:
         assert self._annoy_mapping is not None, "No annoy mapping has been loaded. Call load_entity_index(file_annoy)"
 
         nns_ids = self._annoy_index.get_nns_by_vector(emb_vector, num_nn)
-        nns_entities = set()
+        nns_entities = []
         with self._annoy_mapping.begin() as mapping:
             for id in nns_ids:
-                nns_entities.add(mapping.get(str(id).encode()).decode())
+                nns_entities.append(mapping.get(str(id).encode()).decode())
 
         return nns_entities
 
-    def get_nns_by_phrase(self, phrase: str, num_nn: int) -> Set[str]:
+    def get_nns_by_phrase(self, phrase: str, num_nn: int) -> List[str]:
         """
         Returns a set of nearest neighbour entities for the given phrase.
         """
@@ -93,9 +95,22 @@ class AnnoyIndexer:
         phrase = str(phrase)
         refactored_phrase = phrase.replace("_", " ")
 
-        nns_entities = set()
-        if self._embedding_model_type == 'sent2vec':
-            emb = self._embedding_model.embed_sentence(refactored_phrase)[0]
-            nns_entities = self.get_nns_by_vector(emb, num_nn)
+        emb = self._get_embedding(refactored_phrase)
+        nns_entities = self.get_nns_by_vector(emb, num_nn)
 
         return nns_entities
+
+
+class Sent2VecIndexer(AnnoyIndexer):
+    def __init__(self, embedding_model_path: str):
+        embedding_model = sent2vec.Sent2vecModel()
+        embedding_model.load_model(embedding_model_path)
+        embedding_vector_size = embedding_model.get_emb_size()
+
+        super().__init__(embedding_model, embedding_vector_size)
+
+    def _get_embedding(self, phrase: str) -> List[float]:
+        emb = self._embedding_model.embed_sentence(phrase)[0]
+        if not np.any(emb):
+            log.warning("Phrase '%s' not found in sent2vec. Zero-vector returned." % phrase)
+        return self._embedding_model.embed_sentence(phrase)[0]
